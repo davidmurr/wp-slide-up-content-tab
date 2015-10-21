de516d23b173181e5884eb800776c635be73df54
David Murr <david.murr@gmail.com>
2015-10-21 19:21:45 -0400

    Fixes Issue #1

--- wp-slide-up-content-tab.php
+++ wp-slide-up-content-tab.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Slide Up Content Tab
  * Plugin URI: https://davidmurr.com/
  * Description: Creates a simple sticky slide-up tab that you can use to display any content you'd like!
- * Version: 0.3
+ * Version: 0.4
  * Author: David Murr
  * Author URI: https://davidmurr.com/
  * License: GPL2
@@ -36,8 +36,38 @@ function wp_slide_up_content_tab_settings_page() {
 		echo '
 			<table class="form-table">
 				<tr valign="top">
-					<th scope="row">Post to Display:</th>
-					<td><input type="text" name="dmsuct_display_post_id" value="' . esc_attr( get_option('dmsuct_display_post_id') ) . '" /></td>
+					<th scope="row">Page Content to Display:</th>
+					<td>
+						<select name="dmsuct_display_post_id" id="dmsuct_display_post_id">';
+						$dm_args = array(
+							'posts_per_page'   => 9999,
+							'offset'           => 0,
+							'category'         => '',
+							'category_name'    => '',
+							'orderby'          => 'post_title',
+							'order'            => 'ASC',
+							'include'          => '',
+							'exclude'          => '',
+							'meta_key'         => '',
+							'meta_value'       => '',
+							'post_type'        => 'page',
+							'post_mime_type'   => '',
+							'post_parent'      => '',
+							'author'	   => '',
+							'post_status'      => 'publish',
+							'suppress_filters' => true 
+							);
+							$dm_posts_array = get_posts( $dm_args );
+							foreach ($dm_posts_array as $dm_post) {
+								echo '<option value="' . $dm_post->ID . '"';
+								if ($dm_post->ID == esc_attr( get_option('dmsuct_display_post_id') )) {
+									echo ' selected="selected"';
+								}
+								echo '>' . $dm_post->post_title . '</option>';
+							}
+					echo '
+						</select>
+					</td>
 				</tr>
 			</table>';
 		submit_button();
@@ -54,8 +84,8 @@ function wp_slide_up_content_tab_assets() {
 
 function wp_slide_up_content_tab() {
 	
-	$post_title = apply_filters('the_title', get_post_field('post_title', get_option('dmsuct_display_post_id')));
-	$post_content = apply_filters('the_content', get_post_field('post_content', get_option('dmsuct_display_post_id')));
+	$post_title = apply_filters('the_title', get_post_field('post_title', esc_attr(get_option('dmsuct_display_post_id'))));
+	$post_content = apply_filters('the_content', get_post_field('post_content', esc_attr(get_option('dmsuct_display_post_id'))));
 		
 	echo '<div class="wp_slide_up_content_tab_container">';
 	echo '<h3 class="wp_slide_up_content_tab_grabber">' . $post_title . '</h3>';
